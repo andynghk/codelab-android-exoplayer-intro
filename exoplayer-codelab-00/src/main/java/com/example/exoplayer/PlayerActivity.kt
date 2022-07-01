f63944e5512bb114b7c5ac5734dd04e0b3df5525
Andy Ng <andy.ng@betterpi.com>
2022-07-01 16:52:16 +0800

    Update code by following YouTube video, How to optimize media streaming with ExoPlayer

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -15,14 +15,32 @@
  */
 package com.example.exoplayer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
+import androidx.media3.common.util.Log
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import com.example.exoplayer.databinding.ActivityPlayerBinding
 
 /**
  * A fullscreen activity to play audio or video streams.
  */
 class PlayerActivity : AppCompatActivity() {
+    private var player: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
+
+    private val playbackStateListener: Player.Listener = playbackStateListener()
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityPlayerBinding.inflate(layoutInflater)
@@ -31,5 +49,131 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.let { exoPlayer ->
+            playbackPosition = exoPlayer.currentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.removeListener(playbackStateListener)
+            exoPlayer.release()
+        }
+        player = null
+    }
+
+    private fun initializePlayer() {
+//        Adaptive track selection
+        val trackSelector = DefaultTrackSelector(this).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
+
+        player = ExoPlayer.Builder(this)
+//        Adaptive track selection
+                .setTrackSelector(trackSelector)
+                .build()
+                .also {
+                    exoPlayer -> viewBinding.videoView.player = exoPlayer
+
+//                    Adaptive track selection
+                    val mediaItem = MediaItem.Builder()
+                            .setUri(getString(R.string.media_url_dash))
+                            .setMimeType(MimeTypes.APPLICATION_MPD)
+                            .build()
+                    exoPlayer.setMediaItem(mediaItem)
+
+//                    You can choose to play MP3 or MP4
+//                    val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+                    val thirdMediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+                    exoPlayer.addMediaItem(thirdMediaItem)
+
+//                    You can also create a playlist with MP3 and MP4
+                    val secondMediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+                    exoPlayer.addMediaItem(secondMediaItem)
+
+                    exoPlayer.playWhenReady = playWhenReady
+                    exoPlayer.seekTo(currentItem, playbackPosition)
+                    exoPlayer.addListener(playbackStateListener)
+                    exoPlayer.prepare()
+                }
+    }
+}
+
+private const val TAG = "PlayerActivity"
+
+private fun playbackStateListener() = object : Player.Listener {
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        val stateString: String = when (playbackState) {
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
+            else -> "UNKNOWN_STATE             -"
+        }
+        Log.d(TAG, "changed state to $stateString")
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        super.onIsPlayingChanged(isPlaying)
+        val playingString: String = if (isPlaying) "PLAYING" else "NOT PLAYING"
+        Log.d(TAG, "player is currenly $playingString")
+    }
+
+    override fun onRenderedFirstFrame() {
+        super.onRenderedFirstFrame()
+    }
+
+    override fun onEvents(player: Player, events: Player.Events) {
+        super.onEvents(player, events)
+
+        if (events.contains(Player.EVENT_PLAYBACK_STATE_CHANGED) ||
+            events.contains(Player.EVENT_IS_PLAYING_CHANGED)
+        ) {
+//            Update the UI
+            Log.d(TAG, "player UI needs to be updated")
+        }
+
+        if (events.contains(Player.EVENT_MEDIA_ITEM_TRANSITION)) {
+            player.seekTo(5000L)
+        }
     }
 }
